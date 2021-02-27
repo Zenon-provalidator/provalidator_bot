2e57a2e558dd23bda6d19a3f2834f67820b67d89
zenon <zenon@provalidator.com>
2021-02-27 19:46:15 +0900

    logger

--- provalidator_bot/log4js.js
+++ provalidator_bot/log4js.js
@@ -3,21 +3,21 @@ const log4js = require('log4js')
 //logger
 log4js.configure({
     appenders: { 
-    	provalidator_sif_bot: { 
+    	provalidator_bot: { 
 			type: 'dateFile', 
-			filename: './provalidator_sif_bot.log',
+			filename: './provalidator_bot.log',
 		    compress: true
     	} 
     },
     categories: { 
     	default: { 
-    		appenders: ['provalidator_sif_bot'], 
+    		appenders: ['provalidator_bot'], 
     		level: 'debug' 
     	} 
     }
 })
 
-const logger = log4js.getLogger('provalidator_sif_bot')
+const logger = log4js.getLogger('provalidator_bot')
 
 // console.log(logger)
 module.exports = {
