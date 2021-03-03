bb426d46aac465366a2a601e0980c287e5f11802
zenon-provalidator <zenon@provalidator.com>
2021-03-03 21:28:08 +0900

    update communuty tokens

--- provalidator_bot/func.js
+++ provalidator_bot/func.js
@@ -1,7 +1,8 @@
 const fetch = require('sync-fetch')
 require('dotenv').config()
 const logger = require('./log4js').log4js//logger
-const fs = require('fs');
+const fs = require('fs')
+const numeral = require('numeral')
 
 //let rawdata = fs.readFileSync('student.json');
 //let student = JSON.parse(rawdata);
@@ -11,11 +12,11 @@ const fs = require('fs');
 function getMessage(coin){
 	let msg = ``
 	let price = ``
-	let totalTokens = ``
+	let maxTokens = ``
 	let stakedTokens = ``
-	let notStakedTokens = ``
+	let totalTokens = ``
 	let stakedPercent = ``
-	let notStakedPercent = ``
+	let totalPercent = ``
 	let teamTokens = ``
 	let communityTokens = ``
 	let communityPercent = ``
@@ -34,23 +35,25 @@ function getMessage(coin){
 //				console.log('new' + cdate)
 //				console.log(getCommunityTokens())
 				price = getSifDexPrice().toFixed(4)
-				totalTokens = (getTokenTotal('rowan') / 1000000000000000000).toFixed(0)
+				maxTokens = (getTokenTotal('rowan') / 1000000000000000000).toFixed(0)
 				stakedTokens = (getStaked() / 1000000000000000000).toFixed(0)
-				notStakedTokens = totalTokens - stakedTokens
-				stakedPercent = (stakedTokens / totalTokens * 100).toFixed(0)
-				notStakedPercent = (notStakedTokens / totalTokens * 100).toFixed(0)
+				stakedPercent = (stakedTokens / maxTokens * 100).toFixed(0)
+//				totalTokens = maxTokens - stakedTokens
+//				totalPercent = (totalTokens / maxTokens * 100).toFixed(0)
 				teamTokens = getTeamTokens()
-				teamPercent = (teamTokens / totalTokens * 100).toFixed(0)
+				teamPercent = (teamTokens / maxTokens * 100).toFixed(0)
 				communityTokens = stakedTokens - teamTokens
-				communityPercent = (communityTokens / totalTokens * 100).toFixed(0)
+				communityPercent = (communityTokens / maxTokens * 100).toFixed(0)
+				totalTokens = teamTokens + communityTokens
+				totalPercent = (totalTokens / maxTokens * 100).toFixed(0)
 				
 				let wJson = {
 					"price" : price,
-					"totalTokens" : totalTokens,
+					"maxTokens" : maxTokens,
 					"stakedTokens" : stakedTokens,
-					"notStakedTokens" : notStakedTokens,
+					"totalTokens" : totalTokens,
 					"stakedPercent" : stakedPercent,
-					"notStakedPercent" : notStakedPercent,
+					"totalPercent" : totalPercent,
 					"teamTokens" : teamTokens,
 					"teamPercent" : teamPercent,
 					"communityTokens" : communityTokens,
@@ -61,11 +64,11 @@ function getMessage(coin){
 			}else { //old
 //				console.log('old'+ cdate)
 				price = rJson.price
-				totalTokens = rJson.totalTokens
+				maxTokens = rJson.maxTokens
 				stakedTokens = rJson.stakedTokens
-				notStakedTokens = rJson.notStakedTokens
+				totalTokens = rJson.totalTokens
 				stakedPercent = rJson.stakedPercent
-				notStakedPercent = rJson.notStakedPercent
+				totalPercent = rJson.totalPercent
 				teamTokens = rJson.teamTokens
 				teamPercent = rJson.teamPercent
 				communityTokens = rJson.communityTokens
@@ -76,9 +79,9 @@ function getMessage(coin){
 		msg += `🥩<b>Staking</b>\n\n`
 		msg += `✅Community : ${numberWithCommas(communityTokens)} (${communityPercent}%)\n\n`
 //		msg += `**Team : ${numberWithCommas(teamTokens)} (${teamPercent}%)\n\n`
-		msg += `✅Total : ${numberWithCommas(notStakedTokens)} (${notStakedPercent}%)\n\n`
-		msg += `⛓️Max Sply : ${numberWithCommas(totalTokens)} (100%)\n\n`
-		msg += `📌150M staked by Foundation will be removed soon and is not eligible for validator rewards.\n\n`
+		msg += `✅Total : ${numberWithCommas(totalTokens)} (${totalPercent}%)\n\n`
+		msg += `⛓️Max Sply : ${numberWithCommas(maxTokens)} (100%)\n\n`
+		msg += `📌${numeral(teamTokens).format('0.0a').toUpperCase()} staked by Foundation will be removed soon and is not eligible for validator rewards.\n\n`
 		msg += `ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ\n`
 		msg += `Supported by <a href='https://provalidator.com'>Provalidator</a>\n`
 		return msg
